455cdb0fc70478f9e9949f7fe89a549a624e64ff
meta-abhisheksharma <abhishek.sharma@metacube.com>
2016-08-08 15:40:27 +0530

    Update MyArrayList.java

--- DS_Assignment_1/MyArrayList/MyArrayList.java
+++ DS_Assignment_1/MyArrayList/MyArrayList.java
@@ -139,14 +139,15 @@ public class MyArrayList{
 			System.out.println("You Cannot Perform Delete Operation In Empty List");
 			return;
 		}else{
+			for(int col = 0; col < this.size(); col++){
 			for(int row= 0; row < this.size(); row++){
 				if(array[row] == obj){
 					for(int arrIndex= row; arrIndex < this.size()-1; arrIndex++){
 						array[arrIndex]= array[arrIndex+1];
 					}
 					index--;
-					return;
 				}	
+				col--;
 			}
 			System.out.println("Element Is Not Available");
 		}
